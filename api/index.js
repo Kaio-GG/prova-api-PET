0249e4346ec4aa4f4847502be25a0d757ac7e487
Kaio Felipe <kaiofdss@gmail.com>
2022-06-06 10:37:55 -0300

    ALTERADO

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+
+import cors from 'cors'
+import  express  from 'express'
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+
+server.listen(process.env.PORT, ()=> console.log)
